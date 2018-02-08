a576e2e1c1bfad291452aea1d78f562c86dd03f0
feemonteiro <feemoonteiro@gmail.com>
2018-02-08 09:52:26 -0300

    primeiro commit

---
+++ src/com/bot/elizaBot/ElizaBot.java
@@ -0,0 +1,9 @@
+package com.bot.elizaBot;
+
+public class ElizaBot {
+    public static void main(String[] args){
+        string noise_list[10] = new string[10];
+
+        System.out.println("Teste");
+    }
+}
